bd33cf11b2e8808ea124e3e17cae5ddb0dde82b7
Wojtek <wojciechwojtak@gmail.com>
2016-07-19 21:39:25 +0200

    :wq

--- frontend/server.js
+++ frontend/server.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = express();
-
+app.use(express.static(__dirname + '/app'));
 app.get('/', function (req, res) {
-    res.sendFile('index.html', {root: __dirname});
+    res.sendFile('app/index.html', {root: __dirname});
 });
+
 
 app.listen(3000, function () {
     console.log('OK!');
