8c2cf4cca8246e768bbf59c0593edfe9061cb7da
Wojtek <wojciechwojtak@gmail.com>
2016-07-27 20:37:37 +0200

    tasks modyfication

--- src/main/java/com/todo/tasks/Task.java
+++ src/main/java/com/todo/tasks/Task.java
@@ -3,16 +3,17 @@ package com.todo.tasks;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.data.annotation.*;
 
 import javax.persistence.*;
-
+import javax.persistence.Id;
 
 
 @Entity
 @Table(name = "tasks")
 @ToString
 public class Task {
-
+    @Id
     @Column(name = "tasks")
     @Setter
     @Getter
