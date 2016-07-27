8655db5c82c661da96b0d0194042eabb5e8b2e8a
Wojtek <wojciechwojtak@gmail.com>
2016-07-27 20:44:16 +0200

    tasks modyfication2

--- src/main/java/com/todo/Tasks/TaskDao.java
+++ src/main/java/com/todo/Tasks/TaskDao.java
@@ -2,9 +2,6 @@ package com.todo.tasks;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-/**
- * Created by RENT on 2016-07-26.
- */
 public interface TaskDao extends JpaRepository<Task, Long> {
 }
 
